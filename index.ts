847eb2bfb14c4c6f5eb79dcaf411dbc6a0fd9025
Wiwok <Emil.dechenaud@gmail.com>
2023-06-29 11:07:40 +0200

    Cleaning index

--- index.ts
+++ index.ts
@@ -1,37 +1,50 @@
 import express from 'express';
-import models from './models';
-import adminAction from './admin/Commands';
-import sms from './smsSender';
-import isBan from './admin/checkBan';
-var app = express();
-//express
-const port = 5000
 
-app.post('/', function (req) {
-	// is corect message ?
-	if(typeof req.query.message != 'string' || typeof req.query.contact != 'string'){return}
+import { IsPhoneNumber } from './Utils';
+import adminAction from './admin/Commands';
+import isBan from './admin/checkBan';
+import models from './models';
+import sms from './smsSender';
+import chalk from 'chalk';
+
+const app = express();
+const port = 5000;
+const prefix = '!';
+
+app.post('/', req => {
+	if (typeof req.query.message != 'string' || typeof req.query.contact != 'string') return;
 	let phoneNumber = req.query.contact;
 	let message = req.query.message;
-	//clear request
-	if(phoneNumber.startsWith('+33')){phoneNumber = phoneNumber.replace('+33','0')}
-	//is ban ?
-	if(isBan(phoneNumber)){console.log('user banned : '+phoneNumber+' send message'); return};
-	
-	//command ?
-	if((message).startsWith('!')){
-		console.log('message from '+phoneNumber+' : '+message);
-		message = message.replace('!','');
+
+	if (phoneNumber.startsWith('+33')) phoneNumber = phoneNumber.replace('+33', '0');
+
+
+	if (isBan(phoneNumber)) {
+		console.log('[' + chalk.yellow('USER') + ']	\'' + chalk.bold(phoneNumber) + '\': Sent a message');
+		return;
+	}
+
+	if (!IsPhoneNumber(phoneNumber)) return;
+
+	if (message.startsWith(prefix)) {
+		message = message.replace(prefix, '');
+		console.log('[' + chalk.yellow('COMMAND') + ']	\'' + chalk.bold(phoneNumber) + '\': ' + message);
 		adminAction(phoneNumber, message, myModel, mySms);
-	}else{
-		//ask to llama
-		process.stdout.write('message from '+phoneNumber +' : ');
-		myModel.send(message, (message)=>{console.log('    '+message); mySms.sendSms(phoneNumber, message)});
+	} else {
+		process.stdout.write('[' + chalk.blue('MESSAGE') + ']	\'' + chalk.bold(phoneNumber) + '\': ');
+		myModel.send(message, message => {
+			console.log('[' + chalk.blue('MODEL') + ']	' + message);
+			mySms.sendSms(phoneNumber, message);
+		});
 	}
 });
 
-app.listen(port, () => {
-	console.clear();
-	console.log(`Example app listening on port ${port}`)
-});
+console.clear();
+
 const mySms = new sms();
-const myModel = new models('/home/unel/Documents/llama.cpp/examples/myChat.sh');
+const myModel = new models('../llama.cpp/examples/myChat.sh');
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+	console.log('Ready !');
+});
