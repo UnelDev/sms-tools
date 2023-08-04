d34e5110d81aee5f79228b767bc9cadbfb76f3de
unel <86972822+UnelDev@users.noreply.github.com>
2023-08-04 12:53:56 +0200

    add tools in utils

--- Utils.ts
+++ Utils.ts
@@ -1,3 +1,6 @@
+import admin from "./class/admin";
+import user from "./class/user";
+
 function IsPhoneNumber(number: any) {
 	if (typeof number != 'string') return false;
 	if (!number.match(/^(?:\+33|0)\s*[1-9](?:\s*\d{2}){4}$/)) return false;
@@ -5,4 +8,42 @@ function IsPhoneNumber(number: any) {
 	return true;
 }
 
-export { IsPhoneNumber };
+function isAdminPhoneNumber(adminArray: Array<admin>, phoneNumber: string): boolean {
+	let isAdmin = false;
+	adminArray.forEach((adminInstance) => {
+		if (adminInstance.phoneNumber === phoneNumber) {
+			isAdmin = true;
+		}
+	});
+	return isAdmin;
+}
+
+function isUserPhoneNumber(UserArray: Array<user>, phoneNumber: string): boolean {
+	let isUser = false;
+	UserArray.forEach((UserInstance) => {
+		if (UserInstance.phoneNumber === phoneNumber) {
+			isUser = true;
+		}
+	});
+	return isUser;
+}
+
+function getAdminByPhoneNumber(adminArray: Array<admin>, phoneNumber: string): admin | undefined {
+	adminArray.forEach((adminInstance) => {
+		if (adminInstance.phoneNumber === phoneNumber) {
+			return (adminInstance);
+		}
+	});
+	return (undefined);
+}
+
+function getUserByPhoneNumber(UserArray: Array<user>, phoneNumber: string): admin | undefined {
+	UserArray.forEach((UserInstance) => {
+		if (UserInstance.phoneNumber === phoneNumber) {
+			return (UserInstance);
+		}
+	});
+	return (undefined);
+}
+
+export { IsPhoneNumber, isAdminPhoneNumber, isUserPhoneNumber, getAdminByPhoneNumber, getUserByPhoneNumber };
