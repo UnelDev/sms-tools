ba81d7f4eecfb1156f6783fd6d3c4a7b09fb732d
Wiwok <Emil.dechenaud@gmail.com>
2023-12-18 16:27:38 +0100

    Cleaning boldering method

--- tools/tools.ts
+++ tools/tools.ts
@@ -2,21 +2,23 @@ import Service from '../services/Service';
 import User from '../user/User';
 
 function bolderize(text: string) {
-	let newText = '';
-	for (var i = 0; i < text.length; i++) {
-		const currentCode = text[i].codePointAt(0);
+	return text
+		.split('')
+		.map(char => {
+			const code = char.codePointAt(0);
 
-		if (currentCode >= 97 && currentCode <= 122) {
-			newText += String.fromCodePoint(currentCode + 120_205);
-		} else if (currentCode >= 65 && currentCode <= 90) {
-			newText += String.fromCodePoint(currentCode + 120_211);
-		} else if (currentCode >= 48 && currentCode <= 57) {
-			newText += String.fromCodePoint(currentCode + 120_764);
-		} else {
-			newText += text[i];
-		}
-	}
-	return newText;
+			if (typeof code == 'undefined') return char;
+
+			if (code >= 97 && code <= 122) {
+				return String.fromCodePoint(code + 120_205);
+			} else if (code >= 65 && code <= 90) {
+				return String.fromCodePoint(code + 120_211);
+			} else if (code >= 48 && code <= 57) {
+				return String.fromCodePoint(code + 120_764);
+			}
+			return char;
+		})
+		.join('');
 }
 
 function IsPhoneNumber(number: any) {
