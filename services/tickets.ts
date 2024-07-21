4b9274701287eb8133aba9dac288e475bc6130e3
Wiwok <emil.dechenaud@gmail.com>
2024-07-21 10:48:17 +0200

    Added tickets for zone 1 and 2

--- services/tickets.ts
+++ services/tickets.ts
@@ -12,11 +12,14 @@ class Tickets extends Service {
 		this.name = 'tikect';
 	}
 	newAction(user: User, message: string) {
-		if (message.trim().toLocaleLowerCase() == 'z' || message.trim().toLocaleLowerCase() == '3z')
-			this.threeArea(user);
+		if (message.trim().toLocaleLowerCase() == '1z') this.oneArea(user);
+		else if (message.trim().toLocaleLowerCase() == '2z') this.twoArea(user);
+		else if (message.trim().toLocaleLowerCase() == '3z') this.threeArea(user);
 		else {
 			user.sendMessage(`You have selected the ${bolderize(this.name)} service. List of command:
-${bolderize('3z or z')} : get a ticket (3 zones, 2 hours)
+${bolderize('1z')} : get a ticket (1 zone, 1 hour)
+${bolderize('2z')} : get a ticket (2 zones, 2 hours)
+${bolderize('3z')} : get a ticket (3 zones, 2 hours)
 
 ${bolderize('home')}: Go back to the main menu`);
 		}
@@ -48,6 +51,64 @@ A PRÉSENTER AU CONDUCTEUR
 ${user.phoneNumber.replaceAll('+33', '')}S0XU
 bit.ly/CGVticketSMS
 			`);
+		}
+	}
+
+	oneArea(user: User) {
+		{
+			const date = new Date();
+			const day = date.getDate().toString().padStart(2, '0');
+			const month = (date.getMonth() + 1).toString().padStart(2, '0');
+			const year = date.getFullYear().toString().slice(-2);
+			const hoursEnd = new Date();
+			hoursEnd.setHours(hoursEnd.getHours() + 1);
+			user.sendMessage(`Cars Région
+	Titre 1 zone
+
+	Valable 1h
+	${date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })} à ${hoursEnd.toLocaleTimeString('fr-FR', {
+				hour: '2-digit',
+				minute: '2-digit'
+			})}
+	${day}.${month}.${year}
+	3.90 euros
+
+	A PRÉSENTER AU CONDUCTEUR
+
+	78'52'72'38'99'34
+
+	${user.phoneNumber.replaceAll('+33', '')}S0XU
+	bit.ly/CGVticketSMS
+				`);
+		}
+	}
+
+	twoArea(user: User) {
+		{
+			const date = new Date();
+			const day = date.getDate().toString().padStart(2, '0');
+			const month = (date.getMonth() + 1).toString().padStart(2, '0');
+			const year = date.getFullYear().toString().slice(-2);
+			const hoursEnd = new Date();
+			hoursEnd.setHours(hoursEnd.getHours() + 2);
+			user.sendMessage(`Cars Région
+	Titre 2 zones
+
+	Valable 2h
+	${date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })} à ${hoursEnd.toLocaleTimeString('fr-FR', {
+				hour: '2-digit',
+				minute: '2-digit'
+			})}
+	${day}.${month}.${year}
+	5.40 euros
+
+	A PRÉSENTER AU CONDUCTEUR
+
+	78'52'72'38'99'34
+
+	${user.phoneNumber.replaceAll('+33', '')}S0XU
+	bit.ly/CGVticketSMS
+				`);
 		}
 	}
 }
