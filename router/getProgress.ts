e014f691e35219237a4e579305a0ccaf1e66677c
unel <enee.prog@gmail.com>
2024-12-18 18:08:34 +0100

    add getProgress

---
+++ router/getProgress.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import authenticate from '../tools/authentificate';
+import { Message } from '../models/message.model';
+import { log } from '../tools/log';
+
+async function getProgress(req: Request<any>, res: Response<any>) {
+	req.body.ContactID = '67463daade8609f23e827fb7';
+	const user = authenticate(req, res);
+	if (!user) return;
+
+	const count = await Message.aggregate([
+		{ $match: { direction: false } },
+		{ $group: { _id: '$contactID' } },
+		{ $count: 'uniqueClients' }
+	]);
+
+	res.status(200).send({ message: 'nbUser', data: count[0]?.uniqueClients || 0, OK: true });
+	log('user get the number of client', 'INFO', __dirname, { user }, user.id);
+}
+
+export default getProgress;
--- router/routes.ts
+++ router/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import getMessage from './getMessage';
+import getProgress from './getProgress';
 import login from './login';
 
 const router = Router();
 router.post('/login', login);
 router.post('/getMessage', getMessage);
+router.post('/getProgress', getProgress);
 
 export default router;
